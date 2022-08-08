ae97f8683e67a01aa34068ab9bff84d882fab733
comfortabankwa <110771514+comfortabankwa@users.noreply.github.com>
2022-08-08 11:55:05 +0000

    Create prime_numbers.java

---
+++ prime_numbers.java
@@ -0,0 +1,30 @@
+/*
+Author: Comfort Abankwah
+Student ID: 10982299
+A PROGRAM THAT PRODUCES THE SUM OF ALL PRIME NUMBERS LESS THAN A GIVEN NUMBER
+*/
+
+public class primeNumbers{
+    public static void main(String[] arg){
+        int sum = 0;
+        int number = 100;
+        for(int i = 1; i < number; i++){
+            if(isPrime(i)){
+                sum += i;
+            }
+        }
+        System.out.println("The Sum of Prime Numbers, 1 to 100 is: " + sum);
+    }
+
+    private static boolean isPrime(int num) {
+        if (num == 2){
+           return true;
+        }
+        for (int i = 3; i < num; i++){
+            if (num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
